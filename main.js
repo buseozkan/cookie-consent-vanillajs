59c500d97ce0cfe372d135d597744a4d715e361d
buseozkan <buse1.ozkan@gmail.com>
2022-12-07 18:16:20 +0000

    cookie consent vanilla JS example

---
+++ main.js
@@ -0,0 +1,26 @@
+// if (localStorage.getItem("cookieSeen") != "shown") {
+//   $(".cookie-banner").delay(2000).fadeIn();
+//   localStorage.setItem("cookieSeen", "shown");
+// }
+
+// $(".close").click(function (e) {
+//   $(".cookie-banner").fadeOut();
+// });
+let cookieModal = document.querySelector(".cookie-consent-modal");
+let cancelCookieBtn = document.querySelector(".btn.cancel");
+let acceptCookieBtn = document.querySelector(".btn.accept");
+
+cancelCookieBtn.addEventListener("click", function () {
+  cookieModal.classList.remove("active");
+});
+acceptCookieBtn.addEventListener("click", function () {
+  cookieModal.classList.remove("active");
+  localStorage.setItem("cookieAccepted", "yes");
+});
+
+setTimeout(function () {
+  let cookieAccepted = localStorage.getItem("cookieAccepted");
+  if (cookieAccepted != "yes") {
+    cookieModal.classList.add("active");
+  }
+}, 2000);
